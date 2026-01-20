625d74594e1de4899819e4273a1d693eda5dd190
marlabharghavsai <marlabharghavsai@gmail.com>
2026-01-20 06:03:29 +0000

    Fix: allow Prometheus access on port 9090

--- terraform/main.tf
+++ terraform/main.tf
@@ -60,6 +60,12 @@ resource "aws_security_group" "ec2_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+  from_port   = 9090
+  to_port     = 9090
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  }
 
   egress {
     from_port   = 0
